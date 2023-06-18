f4f1ef5dda5e797ac06b4a0d71b258b45db02f30
jayrel <jayrelstark@gmail.com>
2023-06-18 23:50:58 +0800

    fixed log <a>tag

--- website-clone/src/components/SteamLogo.jsx
+++ website-clone/src/components/SteamLogo.jsx
@@ -4,16 +4,14 @@ import { FaGlobe } from "react-icons/fa";
 
 function SteamLogo() {
   return (
-    <div className="flex flex-row m-auto space-x-3 -space-y-2 text-white text-right ">
-          <button
-            type="button" 
-            className="z-10 border-4 border-gray-500 hover:border-orange-700 font-semibold bg-slate  rounded-lg text-2xl px-3 py-3 inline-flex items-center">
-              <h2 href="#"  className="px-2 "> <FaSteam /></h2>
-            PLAY FOR FREE
-          </button>
-                     
-
-    </div>
+    <a href="https://store.steampowered.com/app/570/Dota_2/" className="flex flex-row m-auto space-x-3 -space-y-2 text-white text-right " >
+            <button
+              type="button"
+              className="z-10 border-4 border-gray-500 hover:border-orange-700 font-semibold bg-slate  rounded-lg text-2xl px-3 py-3 inline-flex items-center">
+                <h2 className="px-2 "> <FaSteam /></h2>
+              PLAY FOR FREE
+            </button>
+          </a>
   );
 }
 export default SteamLogo
